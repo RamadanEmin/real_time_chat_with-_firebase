2f77d49866921992da12e96e999fd91b369931d9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-05 13:49:28 +0300

    Added search user functionality

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,19 +1,51 @@
+import { useState } from 'react';
+import { db } from '../../../../lib/firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useUserStore } from '../../../../lib/userStore';
+
 import './addUser.css';
 
 const AddUser = () => {
+    const [user, setUser] = useState(null);
+
+    const { currentUser } = useUserStore();
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+
+        try {
+            const userRef = collection(db, 'users');
+
+            const q = query(userRef, where('username', '==', username));
+
+            const querySnapShot = await getDocs(q);
+
+            if (!querySnapShot.empty) {
+                setUser(querySnapShot.docs[0].data());
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div className="addUser">
-            <form>
+            <form onSubmit={handleSearch}>
                 <input type="text" placeholder="Username" name="username" />
                 <button>Search</button>
             </form>
+            {user && (
                 <div className="user">
                     <div className="detail">
-                        <img src="./avatar.png" alt="" />
-                        <span>Jane Doe</span>
+                        <img src={user.avatar || './avatar.png'} alt="" />
+                        <span>{user.username}</span>
                     </div>
                     <button>Add User</button>
                 </div>
+            )}
         </div>
     );
 };
