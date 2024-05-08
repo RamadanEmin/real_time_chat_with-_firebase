c2a5f5eafe55c77a960c0ca2b37a06688a4903c3
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-08 14:46:16 +0300

    Added Select chat functionality

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import AddUser from './addUser/AddUser';
 import { useUserStore } from '../../../lib/userStore';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../../lib/firebase';
+import { useChatStore } from '../../../lib/chatStore';
 
 import './chatList.css';
 
@@ -11,6 +12,7 @@ const ChatList = () => {
     const [addMode, setAddMode] = useState(false);
 
     const { currentUser } = useUserStore();
+    const { chatId, changeChat } = useChatStore();
 
     useEffect(() => {
         const unSub = onSnapshot(
@@ -38,6 +40,32 @@ const ChatList = () => {
         };
     }, [currentUser.id]);
 
+
+    const handleSelect = async (chat) => {
+        const userChats = chats.map((item) => {
+            const { user, ...rest } = item;
+            return rest;
+        });
+
+        const chatIndex = userChats.findIndex(
+            (item) => item.chatId === chat.chatId
+        );
+
+        userChats[chatIndex].isSeen = true;
+
+        const userChatsRef = doc(db, 'userchats', currentUser.id);
+
+        try {
+            await updateDoc(userChatsRef, {
+                chats: userChats,
+            });
+            changeChat(chat.chatId, chat.user);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+
     return (
         <div className='chatList'>
             <div className="search">
@@ -56,6 +84,7 @@ const ChatList = () => {
                 <div
                     className="item"
                     key={chat.chatId}
+                    onClick={() => handleSelect(chat)}
                     style={{
                         backgroundColor: chat?.isSeen ? 'transparent' : '#5183fe',
                     }}
