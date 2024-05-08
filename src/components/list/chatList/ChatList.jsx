2a498fd589893c5ef3e7d016b3a3f8d0eab0b7ab
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-08 17:25:49 +0300

    Update Chat list component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -10,6 +10,7 @@ import './chatList.css';
 const ChatList = () => {
     const [chats, setChats] = useState([]);
     const [addMode, setAddMode] = useState(false);
+    const [input, setInput] = useState('');
 
     const { currentUser } = useUserStore();
     const { chatId, changeChat } = useChatStore();
@@ -65,13 +66,20 @@ const ChatList = () => {
         }
     };
 
+    const filteredChats = chats.filter((c) =>
+        c.user.username.toLowerCase().includes(input.toLowerCase())
+    );
 
     return (
         <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
                     <img src="./search.png" alt="" />
-                    <input type="text" placeholder="Search" />
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        onChange={(e) => setInput(e.target.value)}
+                    />
                 </div>
                 <img
                     src={addMode ? './minus.png' : './plus.png'}
@@ -80,7 +88,7 @@ const ChatList = () => {
                     onClick={() => setAddMode((prev) => !prev)}
                 />
             </div>
-            {chats.map((chat) => (
+            {filteredChats.map((chat) => (
                 <div
                     className="item"
                     key={chat.chatId}
@@ -101,7 +109,8 @@ const ChatList = () => {
                         <span>
                             {chat.user.blocked.includes(currentUser.id)
                                 ? 'User'
-                                : chat.user.username}
+                                : chat.user.username
+                            }
                         </span>
                         <p>{chat.lastMessage}</p>
                     </div>
